0e1f44d1209c057f24c7b1f5d9f8ba60e537978e
Rahul Pothula <rahulpothula5@gmail.com>
2024-12-09 01:12:03 +0530

    This is just the basic structure

---
+++ src/main/java/com/rahul/productservice/models/Product.java
@@ -0,0 +1,10 @@
+package com.rahul.productservice.models;
+
+public class Product {
+    private Long id;
+    private String title;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private Category category;
+}
